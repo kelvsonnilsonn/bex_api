11cacfcf6cfbc51608950e2706455728238ec626
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 14:54:21 -0300

    feat(mapper): add 'toInformation' default method in UserMapper

---
+++ src/main/java/com/ecommerce/bex/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.ecommerce.bex.mapper;
+
+import com.ecommerce.bex.dto.UserResponseDTO;
+import com.ecommerce.bex.dto.auth.RegisterRequestDTO;
+import com.ecommerce.bex.model.User;
+import com.ecommerce.bex.model.valueobjects.user.*;
+import org.mapstruct.Mapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserResponseDTO toResponse(User user);
+
+    default UserInformation toInformation(RegisterRequestDTO dto, PasswordEncoder passwordEncoder) {
+        Password password = Password.of(dto.password(), passwordEncoder);
+        Email email = new Email(dto.email());
+        CPF cpf = new CPF(dto.cpf());
+        Zipcode zipcode = new Zipcode(dto.zipcode());
+        Address address = new Address(dto.country(), dto.city(), dto.neighborhood(), dto.street(), dto.number(), zipcode);
+        return new UserInformation(dto.username(), password, email, cpf, address);
+    }
+}
