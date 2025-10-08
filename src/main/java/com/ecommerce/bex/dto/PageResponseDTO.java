82d709377febed306298ca68ac00ea15f9379f02
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 11:31:15 -0300

    feat(dto): add PageResponseDTO to store pageable responses of the system

---
+++ src/main/java/com/ecommerce/bex/dto/PageResponseDTO.java
@@ -0,0 +1,26 @@
+package com.ecommerce.bex.dto;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record PageResponseDTO <T>(
+        List<T> content,
+        int actualPage,
+        int totalPages,
+        long totalItems,
+        int itemsShowedInPage,
+        int httpStatus
+) {
+    public static <T> PageResponseDTO<T> fromPage(Page<T> page){
+        return new PageResponseDTO<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getTotalPages(),
+                page.getTotalElements(),
+                page.getSize(),
+                HttpStatus.OK.value()
+        );
+    }
+}
