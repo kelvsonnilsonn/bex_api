b203b8982da44e96bd47721fa00e6816bbb82470
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 14:46:10 -0300

    feat(handler): add new exceptions into GlobalHandlerException

--- src/main/java/com/ecommerce/bex/handler/GlobalHandlerException.java
+++ src/main/java/com/ecommerce/bex/handler/GlobalHandlerException.java
@@ -1,6 +1,6 @@
 package com.ecommerce.bex.handler;
 
-import com.ecommerce.bex.exception.InvalidCategoryException;
+import com.ecommerce.bex.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +12,30 @@ public class GlobalHandlerException {
     @ExceptionHandler(InvalidCategoryException.class)
     public ResponseEntity<String> handleInvalidCategory(InvalidCategoryException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(FailedLoginAttemptException.class)
+    public ResponseEntity<String> handleLoginFail(FailedLoginAttemptException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleInvalidCategory(UserNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleInvalidCategory(UserAlreadyExistsException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(NotSellerException.class)
+    public ResponseEntity<String> handleInvalidCategory(NotSellerException e){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 }
