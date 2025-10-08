470a12e436d18582251bff976e522fb74dba6b0f
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 14:57:36 -0300

    feat(util): add authentication paths in AppConstants

--- src/main/java/com/ecommerce/bex/util/AppConstants.java
+++ src/main/java/com/ecommerce/bex/util/AppConstants.java
@@ -6,9 +6,12 @@ import lombok.experimental.UtilityClass;
 public class AppConstants {
 
     public static final String USER_BASE_PATH = "/users";
-
+    public static final String AUTH_BASE_PATH = "/auth";
     public static final String PRODUCT_BASE_PATH = "/product";
 
     public static final String ID_PATH = "/{id}";
     public static final String CATEGORY_SEARCH_PATH = "/category/{productCategory}";
+
+    public static final String LOGIN_PATH = "/login";
+    public static final String REGISTER_PATH = "/login";
 }
