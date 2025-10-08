0d17d7cae1444b1e8fc79d3bbb10ed809d3dc7d1
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 15:15:06 -0300

    refactor(exception): remove NotSellerException.java exception

--- src/main/java/com/ecommerce/bex/exception/NotSellerException.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.bex.exception;
-
-public class NotSellerException extends RuntimeException {
-    public NotSellerException() { super("Apenas vendedores podem adicionar um produto"); }
-    public NotSellerException(String message) {
-        super(message);
-    }
-}
