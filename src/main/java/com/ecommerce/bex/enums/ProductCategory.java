6788c76e64ae031cdf6203acdc49319aab413673
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 11:35:52 -0300

    feat(enums): add ProductCategory enum which contains all product category types

---
+++ src/main/java/com/ecommerce/bex/enums/ProductCategory.java
@@ -0,0 +1,22 @@
+package com.ecommerce.bex.enums;
+
+public enum ProductCategory {
+    ELETRONICOS,
+    ELETRODOMESTICOS,
+    INFORMATICA,
+    CELULARES,
+    MODA,
+    BELEZA,
+    CASA,
+    ESPORTE,
+    LIVROS,
+    BRINQUEDOS,
+    AUTOMOTIVO,
+    FERRAMENTAS,
+    SAUDE,
+    PET,
+    SUPERMERCADO,
+    JOGOS,
+    MUSICA,
+    FILMES
+}
