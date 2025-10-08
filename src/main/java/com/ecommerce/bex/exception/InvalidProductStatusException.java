bd3386fb0db975074b31bbd87d8400a839278211
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 11:30:40 -0300

    refactor(exception): remove InvalidProductStatusException.java because will not use it now

--- src/main/java/com/ecommerce/bex/exception/InvalidProductStatusException.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.bex.exception;
-
-public class InvalidProductStatusException extends RuntimeException {
-    public InvalidProductStatusException() { super("O produto já foi entregue"); }
-    public InvalidProductStatusException(String message) {
-        super(message);
-    }
-}
