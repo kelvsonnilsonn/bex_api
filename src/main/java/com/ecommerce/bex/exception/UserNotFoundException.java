bc30e76fc774307e6a0b729eaa7ec80ea3cb0de0
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 14:45:43 -0300

    feat(exception): add UserNotFoundException personalized exception

---
+++ src/main/java/com/ecommerce/bex/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ecommerce.bex.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() { super("Usuário não encontrado"); }
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
