b350817890c68a218ff2cc9bc30c4978672e36bb
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 11:28:36 -0300

    remove run bean to use spring web

--- src/main/java/com/ecommerce/bex/BexApplication.java
+++ src/main/java/com/ecommerce/bex/BexApplication.java
@@ -1,14 +1,7 @@
 package com.ecommerce.bex;
 
-import com.ecommerce.bex.model.valueobjects.user.*;
-import com.ecommerce.bex.service.ProductService;
-import com.ecommerce.bex.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class BexApplication {
@@ -16,12 +9,5 @@ public class BexApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BexApplication.class, args);
 	}
-
-    @Bean
-    public CommandLineRunner run(ProductService userService){
-        return args -> {
-            userService.create();
-        };
-    }
 
 }
