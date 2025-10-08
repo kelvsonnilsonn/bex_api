a030a7acc79d77b2ff2455f9666c53896ec87478
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 14:54:56 -0300

    feat(repository): add 'findByUsername' method in UserRepository

--- src/main/java/com/ecommerce/bex/repository/UserRepository.java
+++ src/main/java/com/ecommerce/bex/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.ecommerce.bex.repository;
 
 import com.ecommerce.bex.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.userInformation.username = :username")
+    Optional<User> findByUsername(String username);
 }
