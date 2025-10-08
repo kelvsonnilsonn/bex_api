9f748a20598069e4dcadd7dbc858563539da6ce7
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 14:44:35 -0300

    feat(service): add SecurityService to manage authentication and security operations
    - contains login method
    - contains register method

---
+++ src/main/java/com/ecommerce/bex/service/SecurityService.java
@@ -0,0 +1,51 @@
+package com.ecommerce.bex.service;
+
+import com.ecommerce.bex.dto.auth.AuthResponseDTO;
+import com.ecommerce.bex.dto.auth.LoginRequestDTO;
+import com.ecommerce.bex.dto.auth.RegisterRequestDTO;
+import com.ecommerce.bex.exception.FailedLoginAttemptException;
+import com.ecommerce.bex.exception.UserAlreadyExistsException;
+import com.ecommerce.bex.mapper.UserMapper;
+import com.ecommerce.bex.model.User;
+import com.ecommerce.bex.model.valueobjects.user.*;
+import com.ecommerce.bex.repository.UserRepository;
+import com.ecommerce.bex.security.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SecurityService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
+    private final TokenService tokenService;
+
+    @Transactional(readOnly = true)
+    public AuthResponseDTO login(LoginRequestDTO dto){
+        User user = userRepository.findByUsername(dto.username()).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        if(!(passwordEncoder.matches(dto.password(), user.getPassword()))){
+            throw new FailedLoginAttemptException();
+        }
+        String token = tokenService.generateToken(user);
+        return new AuthResponseDTO(token, user.getUsername());
+    }
+
+    @Transactional
+    public AuthResponseDTO register(RegisterRequestDTO dto){
+        Optional<User> user = userRepository.findByUsername(dto.username());
+        if(user.isPresent()){
+            throw new UserAlreadyExistsException();
+        }
+        UserInformation userInformation = userMapper.toInformation(dto, passwordEncoder);
+        User userToSave = new User(userInformation);
+        userRepository.save(userToSave);
+        String token = tokenService.generateToken(userToSave);
+        return new AuthResponseDTO(token, userToSave.getUsername());
+    }
+}
