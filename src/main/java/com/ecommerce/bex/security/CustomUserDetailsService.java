9b7adb2c335152c27bb27b15e7bf8d35e9136adf
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 14:41:39 -0300

    feat(security): add CustomUserDetailsService to Security

---
+++ src/main/java/com/ecommerce/bex/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.ecommerce.bex.security;
+
+import com.ecommerce.bex.exception.UserNotFoundException;
+import com.ecommerce.bex.model.User;
+import com.ecommerce.bex.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
