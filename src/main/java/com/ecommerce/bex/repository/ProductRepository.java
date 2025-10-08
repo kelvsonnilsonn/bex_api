c8db185ef6fe73e6dc7b30353f89e27552bf1b3c
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 11:39:43 -0300

    feat(repository): add findByCategory method in ProductRepository

--- src/main/java/com/ecommerce/bex/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/bex/repository/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.ecommerce.bex.repository;
 
+import com.ecommerce.bex.enums.ProductCategory;
 import com.ecommerce.bex.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.category = :category")
+    Page<Product> findByCategory(Pageable pageable, ProductCategory category);
 }
