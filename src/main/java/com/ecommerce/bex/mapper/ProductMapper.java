2f70f93a1ea39398044978a853ea18b64b1b627f
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-08 11:38:40 -0300

    feat(mapper): add ProductMapper to map product in ProductResponseDTO
    - add ProductCreateRequestDTO in ProductInformation

---
+++ src/main/java/com/ecommerce/bex/mapper/ProductMapper.java
@@ -0,0 +1,22 @@
+package com.ecommerce.bex.mapper;
+
+import com.ecommerce.bex.dto.product.ProductCreateRequestDTO;
+import com.ecommerce.bex.dto.product.ProductResponseDTO;
+import com.ecommerce.bex.model.Product;
+import com.ecommerce.bex.model.valueobjects.product.ProductInformation;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(target = "name", source = "productInformation.name")
+    @Mapping(target = "description", source = "productInformation.description")
+    @Mapping(target = "price", source = "productInformation.price")
+    @Mapping(target = "image", source = "productInformation.image")
+    @Mapping(target = "stock", source = "productInformation.stock")
+    @Mapping(target = "sellerId", source = "productInformation.sellerId")
+    ProductResponseDTO toResponse(Product product);
+
+    ProductInformation toInformation(ProductCreateRequestDTO dto);
+}
