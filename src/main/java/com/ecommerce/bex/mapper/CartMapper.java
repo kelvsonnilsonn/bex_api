f33d09c76c2cdbb06ed687a8407fc7a76fc20a4a
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-09 16:01:51 -0300

    feat(mapper): add CartMapper to map Cart in CartResponseDTO and CartItem into ItemResponseDTO

---
+++ src/main/java/com/ecommerce/bex/mapper/CartMapper.java
@@ -0,0 +1,24 @@
+package com.ecommerce.bex.mapper;
+
+import com.ecommerce.bex.dto.cart.CartResponseDTO;
+import com.ecommerce.bex.dto.cart.ItemResponseDTO;
+import com.ecommerce.bex.model.Cart;
+import com.ecommerce.bex.model.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CartMapper {
+
+    @Mapping(target = "items", expression = "java(cart.getItemsSize())")
+    @Mapping(target = "user", expression = "java(cart.getUserId())")
+    @Mapping(target =  "total", expression = "java(cart.calculateTotal())")
+    CartResponseDTO toResponse(Cart cart);
+
+    @Mapping(target = "user", expression = "java(item.getCartUserId())")
+    @Mapping(target = "productName", expression = "java(item.getProductName())")
+    @Mapping(target = "sellerId", expression = "java(item.getSellerId())")
+    @Mapping(target = "productId", expression = "java(item.getProductId())")
+    @Mapping(target = "total", expression = "java(item.getTotalPrice())")
+    ItemResponseDTO toItemResponse(CartItem item);
+}
