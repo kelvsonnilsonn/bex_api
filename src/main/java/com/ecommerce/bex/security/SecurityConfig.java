bb92a647367d8229d5c699b6c1c2134ec1472757
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-09 16:15:21 -0300

    refactor(security): change 'auth' request matchers from "/auth/login" and "/auth/register" to "/auth/**"

--- src/main/java/com/ecommerce/bex/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/bex/security/SecurityConfig.java
@@ -29,8 +29,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/swagger-ui.html").permitAll()
