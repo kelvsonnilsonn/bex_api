cfcba693ddcbf98e9b67147b608ff2205d5f6aaa
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-13 21:31:56 -0300

    feat(dto): add UserEventsIntervalDTO to store user interval inputs to find events by interval and by user id

---
+++ src/main/java/com/ecommerce/bex/dto/UserEventsIntervalDTO.java
@@ -0,0 +1,14 @@
+package com.ecommerce.bex.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record UserEventsIntervalDTO(
+        Long userId,
+        @JsonFormat(pattern = "dd-MM-yyyy'T'HH:mm:ss")
+        LocalDateTime startDate,
+        @JsonFormat(pattern = "dd-MM-yyyy'T'HH:mm:ss")
+        LocalDateTime endDate
+) {
+}
