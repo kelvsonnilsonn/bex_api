3568ba846497fae00ac3791768c02ae03da28b0b
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-13 21:33:46 -0300

    feat(util): add Forbidden response in HttpConstants

--- src/main/java/com/ecommerce/bex/util/HttpConstants.java
+++ src/main/java/com/ecommerce/bex/util/HttpConstants.java
@@ -10,11 +10,13 @@ public class HttpConstants {
     public static final String NO_CONTENT = "204";
     public static final String BAD_REQUEST = "400";
     public static final String UNAUTHORIZED = "401";
+    public static final String FORBIDDEN = "403";
     public static final String NOT_FOUND = "404";
     public static final String CONFLICT = "409";
     public static final String SERVER_ERROR = "500";
 
     public static final String UNAUTHORIZED_MSG = "Sem permissão";
+    public static final String FORBIDDEN_MSG = "Acesso negado";
     public static final String BAD_REQUEST_MSG = "Dados inválidos fornecidos na requisição";
     public static final String INTERN_SERVER_ERROR_MSG = "Erro interno do servidor";
     public static final String NOT_FOUND_MSG = "Informação não encontrada";
