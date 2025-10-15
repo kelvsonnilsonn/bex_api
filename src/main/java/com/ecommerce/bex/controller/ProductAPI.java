0be66ba4cca4d3fbd866c548f8c0532a59514881
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-15 11:02:22 -0300

    feat(controller): defined new endpoints in ProductAPI
    - add delete product endpoint
    - add updateName endpoint
    - add updatePrice endpoint
    - add updateStock endpoint

--- src/main/java/com/ecommerce/bex/controller/ProductAPI.java
+++ src/main/java/com/ecommerce/bex/controller/ProductAPI.java
@@ -1,6 +1,6 @@
 package com.ecommerce.bex.controller;
 
-import com.ecommerce.bex.command.product.CreateProductCommand;
+import com.ecommerce.bex.command.product.*;
 import com.ecommerce.bex.dto.PageResponseDTO;
 import com.ecommerce.bex.dto.ProductResponseDTO;
 import com.ecommerce.bex.util.HttpConstants;
@@ -21,6 +21,34 @@ public interface ProductAPI {
     @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<Long> create(@RequestBody @Valid CreateProductCommand dto);
+
+    @Operation(summary = "Deletar produto", description = "Endpoint para deletar um produto do sistema")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Produto deletado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<Void> delete(@RequestBody @Valid DeleteProductCommand command);
+
+    @Operation(summary = "Atualizar nome do produto", description = "Endpoint para atualizar o nome de um produto")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Nome do produto atualizado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<Void> updateName(@RequestBody @Valid UpdateProductNameCommand command);
+
+    @Operation(summary = "Atualizar preço do produto", description = "Endpoint para atualizar o preço de um produto")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Preço do produto atualizado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<Void> updatePrice(@RequestBody @Valid UpdateProductPriceCommand command);
+
+    @Operation(summary = "Atualizar estoque do produto", description = "Endpoint para atualizar o estoque de um produto")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Estoque do produto atualizado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<Void> updateStock(@RequestBody @Valid UpdateProductStockCommand command);
 
     @Operation(summary = "Listar todos os produtos", description = "Retorna uma lista paginada de todos os produtos cadastrados no sistema")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Lista de produtos retornada com sucesso")
