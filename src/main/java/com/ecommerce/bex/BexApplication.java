282cc091a74e2508da66bdb0ca9ddf94017d4378
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-15 10:51:49 -0300

    feat: add @EnableCaching annotation in BexApplication

--- src/main/java/com/ecommerce/bex/BexApplication.java
+++ src/main/java/com/ecommerce/bex/BexApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.bex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BexApplication {
 
 	public static void main(String[] args) {
