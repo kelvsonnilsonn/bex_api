62a1bf7396e694e6bdfef2129a1e3fddaaa1f745
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-15 10:52:50 -0300

    feat(controller): define new endpoints in CartAPI
    - add RemoveItem endpoint
    - add ClearCart endpoint

--- src/main/java/com/ecommerce/bex/controller/CartAPI.java
+++ src/main/java/com/ecommerce/bex/controller/CartAPI.java
@@ -1,6 +1,7 @@
 package com.ecommerce.bex.controller;
 
 import com.ecommerce.bex.command.cart.AddItemToCartCommand;
+import com.ecommerce.bex.command.cart.RemoveItemFromCartCommand;
 import com.ecommerce.bex.dto.PageResponseDTO;
 import com.ecommerce.bex.dto.CartResponseDTO;
 import com.ecommerce.bex.dto.ItemResponseDTO;
@@ -21,6 +22,18 @@ public interface CartAPI {
     @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<Void> addItem(@RequestBody AddItemToCartCommand command);
+
+    @Operation(summary = "Remover item do carrinho", description = "Endpoint para remover um item do carrinho de compras")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Item removido do carrinho com sucesso")
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<Void> removeItem(@RequestBody RemoveItemFromCartCommand command);
+
+    @Operation(summary = "Limpar carrinho", description = "Endpoint para limpar todos os itens do carrinho de compras")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Carrinho limpo com sucesso")
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<Void> clearCart();
 
     @Operation(summary = "Listar itens do meu carrinho", description = "Endpoint para listar todos os itens do carrinho do usuário autenticado")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Itens listados com sucesso")
