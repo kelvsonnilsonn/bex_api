d783b09b2b5933eff5bdf1a754293002afab415f
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-16 18:29:47 -0300

    feat(repository): add new queries in UserRepository to find user by name, email and cpf

--- src/main/java/com/ecommerce/bex/repository/UserRepository.java
+++ src/main/java/com/ecommerce/bex/repository/UserRepository.java
@@ -8,6 +8,13 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE u.userInformation.username = :username")
+    @Query("SELECT u FROM User u WHERE u.userInformation.username.username = :username")
     Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.userInformation.email.email = :email")
+    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.userInformation.cpf.cpf = :cpf")
+    Optional<User> findByCpf(String cpf);
+
 }
