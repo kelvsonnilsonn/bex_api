0626ae7923821cd8a9268696b390dbaf783647a8
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-16 18:27:11 -0300

    refactor(model): remove user package

--- src/main/java/com/ecommerce/bex/model/valueobjects/UserInformation.java
+++ src/main/java/com/ecommerce/bex/model/valueobjects/UserInformation.java
@@ -1,17 +1,19 @@
-package com.ecommerce.bex.model.valueobjects.user;
+package com.ecommerce.bex.model.valueobjects;
 
+import com.ecommerce.bex.model.valueobjects.cpf.CPF;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInformation {
-    @Getter
-    private String username;
+    private Username username;
 
     @Embedded
     private Password password;
@@ -25,6 +27,20 @@ public class UserInformation {
     @Getter
     @Embedded
     private Address address;
+
+    public void changeUsername(String newName){
+        this.username = Username.of(newName);
+    }
+
+    public void changePassword(String newPassword, PasswordEncoder encoder){
+        this.password = Password.of(newPassword, encoder);
+    }
+
+    public void changeEmail(String newEmail){
+        this.email = Email.of(newEmail);
+    }
+
+    public String getUsername(){ return username.getUsername(); }
 
     public String getEmail(){
         return email.getEmail();
