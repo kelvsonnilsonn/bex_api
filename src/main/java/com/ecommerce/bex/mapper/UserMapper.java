da29da785357ccbdcb5f8bba6b859a93e9dd0eb0
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-16 18:27:59 -0300

    feat(mapper): add toResponse in UserMapper

--- src/main/java/com/ecommerce/bex/mapper/UserMapper.java
+++ src/main/java/com/ecommerce/bex/mapper/UserMapper.java
@@ -1,12 +1,20 @@
 package com.ecommerce.bex.mapper;
 
 import com.ecommerce.bex.command.RegisterCommand;
-import com.ecommerce.bex.model.valueobjects.user.*;
+import com.ecommerce.bex.dto.UserResponseDTO;
+import com.ecommerce.bex.model.User;
+import com.ecommerce.bex.model.valueobjects.*;
+import com.ecommerce.bex.model.valueobjects.cpf.CPF;
+import com.ecommerce.bex.model.valueobjects.zipcode.Zipcode;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(target = "username", expression = "java(user.getUsername())")
+    UserResponseDTO toResponse(User user);
 
     default UserInformation toInformation(RegisterCommand dto, PasswordEncoder passwordEncoder) {
         Password password = Password.of(dto.password(), passwordEncoder);
@@ -14,6 +22,7 @@ public interface UserMapper {
         CPF cpf = new CPF(dto.cpf());
         Zipcode zipcode = new Zipcode(dto.zipcode());
         Address address = new Address(dto.country(), dto.city(), dto.neighborhood(), dto.street(), dto.number(), zipcode);
-        return new UserInformation(dto.username(), password, email, cpf, address);
+        Username username = new Username(dto.username());
+        return new UserInformation(username, password, email, cpf, address);
     }
 }
