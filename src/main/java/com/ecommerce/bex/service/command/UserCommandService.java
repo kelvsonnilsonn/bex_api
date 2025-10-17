af98d02091691b4a48110acaa004fcf188d187ac
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-17 19:19:24 -0300

    feat(service): add @CacheEvict in UserCommandService when update user information
    - add private validate methods to validate if username or email is already in use

--- src/main/java/com/ecommerce/bex/service/command/UserCommandService.java
+++ src/main/java/com/ecommerce/bex/service/command/UserCommandService.java
@@ -4,17 +4,14 @@ import com.ecommerce.bex.command.user.UpdateEmailCommand;
 import com.ecommerce.bex.command.user.UpdateUsernameByIdCommand;
 import com.ecommerce.bex.command.user.UpdateUsernameCommand;
 import com.ecommerce.bex.enums.UserRole;
-import com.ecommerce.bex.exception.InvalidEmailException;
-import com.ecommerce.bex.exception.SmallPrivilegesException;
-import com.ecommerce.bex.exception.UserNotFoundException;
+import com.ecommerce.bex.exception.*;
 import com.ecommerce.bex.model.User;
 import com.ecommerce.bex.repository.UserRepository;
 import com.ecommerce.bex.service.AuthenticationInformation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,27 +21,46 @@ public class UserCommandService {
     private final UserRepository userRepository;
     private final AuthenticationInformation authenticationInformation;
 
+    @CacheEvict(value="users", key="#user.id")
     public void update(UpdateUsernameCommand command){
         User user = authenticationInformation.getAuthenticatedUser();
+        validateUsername(command.newName(), user.getId());
         user.changeUsername(command.newName());
         userRepository.save(user);
     }
 
+    @CacheEvict(value="users", key="#user.id")
     public void update(UpdateUsernameByIdCommand command){
         User user = userRepository.findById(command.id()).orElseThrow(UserNotFoundException::new);
         if(user.getRole().equals(UserRole.ADMIN_ROLE)){
             throw new SmallPrivilegesException();
         }
+        validateUsername(command.newName(), user.getId());
         user.changeUsername(command.newName());
         userRepository.save(user);
     }
 
+    @CacheEvict(value="users", key="#user.id")
     public void update(UpdateEmailCommand command){
         User user = authenticationInformation.getAuthenticatedUser();
-        if(!command.oldEmail().equals(user.getEmail())){
-            throw new InvalidEmailException("Este não é o seu email");
-        }
+        validateEmail(command.newEmail(), user.getId());
         user.changeEmail(command.newEmail());
         userRepository.save(user);
+    }
+
+    private void validateUsername(String username, Long userId){
+        userRepository.findByUsername(username).ifPresent(userInDb -> {
+            if(!userInDb.getId().equals(userId)){
+                throw new UsernameAlreadyInUseException();
+            }
+        });
+    }
+
+    private void validateEmail(String email, Long userId){
+        userRepository.findByEmail(email).ifPresent(userInDb -> {
+            if(!userInDb.getId().equals(userId)){
+                throw new EmailAlreadyInUseException();
+            }
+        });
     }
 }
