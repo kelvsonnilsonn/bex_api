16e97ecabbaa0fd03472f1072d81b1b486c57bf9
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-26 15:12:52 -0300

    feat(command): add @Positive annotation in address number in RegisterCommand

--- src/main/java/com/ecommerce/bex/command/RegisterCommand.java
+++ src/main/java/com/ecommerce/bex/command/RegisterCommand.java
@@ -3,6 +3,7 @@ package com.ecommerce.bex.command;
 import com.ecommerce.bex.model.valueobjects.validation.*;
 import com.ecommerce.bex.util.AppConstants;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record RegisterCommand(
         @NotNull(message = AppConstants.USERNAME_NOT_NULL_MESSAGE) @ValidUsername String username,
@@ -14,7 +15,7 @@ public record RegisterCommand(
         String city,
         String neighborhood,
         String country,
-        int number,
+        @Positive(message = AppConstants.POSITIVE_ADDRESS_NUMBER_MESSAGE) int number,
         @ValidZipcode String zipcode
 ) {
 }
