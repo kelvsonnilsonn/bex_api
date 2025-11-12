6b9b6589b7e8d812a9f916ec90f611ef29a7f500
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-11-12 16:44:28 -0300

    refactor(exception): remove unnecessarily exceptions

--- src/main/java/com/ecommerce/bex/exception/SmallPrivilegesException.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.bex.exception;
-
-public class SmallPrivilegesException extends RuntimeException {
-    public SmallPrivilegesException() {super("Te falta permissão");}
-    public SmallPrivilegesException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/ecommerce/bex/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.bex.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException() { super("O usuário já existe"); }
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
